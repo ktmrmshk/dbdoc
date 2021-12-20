e7966bc4676494316e8973ff0e94f32b4d911d9b
ktmrmshk <ktmrmshk>
2021-12-20 04:16:22 +0000

    added

---
+++ notebooks/src/Snippet/Autoloader_Image_Files.py
@@ -0,0 +1,212 @@
+# Databricks notebook source
+# MAGIC %md ## Autoloaderを使用して、Object Storageにアップロードされたファイル(新規に追加されたファイルのみ)をロードする
+
+# COMMAND ----------
+
+# MAGIC %md ### A: Object Storage上から読み込み、処理を実施して、結果をDelta Lakeに入れる。
+# MAGIC 
+# MAGIC 途中で、Rawデータ様のDeltaテーブルを作成しないパターン
+
+# COMMAND ----------
+
+# clean for retry
+dbutils.fs.rm('/tmp/masahiko.kitamura@databricks.com/', True)
+
+# COMMAND ----------
+
+# 初期化設定
+sql('set spark.databricks.delta.properties.defaults.autoOptimize.optimizeWrite = true;')
+sql('set spark.databricks.delta.properties.defaults.autoOptimize.autoCompact = true;')
+
+# UDFを定義(PILを使用して画像ファイルを開き、属性情報を抽出し、返す)
+@udf("width long, height long, mode string")
+def proc_image(image_binary):
+  import io
+  from PIL import Image
+  f=io.BytesIO(image_binary)
+  im = Image.open(f)
+  return (im.width, im.height, im.mode)
+
+
+
+# Auto Loaderで読み込む (新規ファイルのみ読み込まれる)
+df_autoloader = (
+  spark.readStream.format('cloudFiles')
+  .option('cloudFiles.format', 'binaryFile')
+  .load('s3a://databricks-ktmr-s3/sample/jpg/*.jpg')
+)
+
+# 画像処理(上記で定義したUDFを適用する)
+df_processed = (
+  df_autoloader
+  .withColumn('added_cols', proc_image('content')) # UDFの適用
+  .select(df_autoloader.columns + ['added_cols.*']) # カラムのフラット化
+)
+
+# DeltaLakeに書き出す
+spark.conf.set("spark.sql.parquet.compression.codec", "uncompressed")
+(
+  df_processed.writeStream.format('delta')
+  .option('checkpointLocation', '/tmp/masahiko.kitamura@databricks.com/image.checkpoint')
+  .outputMode('append')
+  .trigger(once=True) # 一度だけ処理
+  .start('/tmp/masahiko.kitamura@databricks.com/image.delta')
+  .awaitTermination() # async => sync
+)
+spark.conf.set("spark.sql.parquet.compression.codec", "snappy")
+
+
+# COMMAND ----------
+
+# 上記で書き出したDeltalakeの内容を確認
+df = spark.read.format('delta').load('/tmp/masahiko.kitamura@databricks.com/image.delta')
+display( df )
+
+# COMMAND ----------
+
+# pandasなどで利用する (10レコードのみ)
+
+pandas_df = df.limit(10).toPandas()
+pandas_df
+
+# COMMAND ----------
+
+# spark Dataframeから一つのカラムを抜き出して、プレビューする
+from matplotlib.pyplot import imshow
+import numpy as np
+from PIL import Image
+import io
+
+
+image_binary = df.filter('path = "s3a://databricks-ktmr-s3/sample/jpg/img200kb_0014.jpg"').collect()[0]['content']
+image_pil = Image.open(io.BytesIO(image_binary))
+imshow(np.asarray(image_pil))
+
+# COMMAND ----------
+
+# MAGIC %md ### B: Object Storage上から読み込み、一度Rawテーブル化を経由して、処理結果をDelta Lakeに入れる。
+# MAGIC 
+# MAGIC 途中で、Rawデータ様のDeltaテーブルを作成するパターン。以下の2つのDelta lakeテーブルが作られる。
+# MAGIC 
+# MAGIC -------
+# MAGIC 
+# MAGIC 1. Rawテーブル(生データのテーブル): `raw_data.delta`
+# MAGIC 2. 画像処理結果のみのテーブル: `image_processed_result.delta`
+# MAGIC 
+# MAGIC -----
+# MAGIC 
+# MAGIC Rawテーブルが作られるので(生データの永続化がDeltalake内に閉じる)、他の用途(パイプラインの分岐)などに対応しやすくなる。
+
+# COMMAND ----------
+
+# DBTITLE 1,Object Storage -> Raw_data table
+# Auto Loaderで読み込む (新規ファイルのみ読み込まれる)
+df_autoloader = (
+  spark.readStream.format('cloudFiles')
+  .option('cloudFiles.format', 'binaryFile')
+  .load('s3a://databricks-ktmr-s3/sample/jpg/*.jpg')
+)
+
+# DeltaLakeに書き出す
+spark.conf.set("spark.sql.parquet.compression.codec", "uncompressed")
+(
+  df_autoloader.writeStream.format('delta')
+  .option('checkpointLocation', '/tmp/masahiko.kitamura@databricks.com/raw_data.checkpoint')
+  .outputMode('append')
+  .trigger(once=True) # 一度だけ処理
+  .start('/tmp/masahiko.kitamura@databricks.com/raw_data.delta')
+  .awaitTermination() # async => sync
+)
+spark.conf.set("spark.sql.parquet.compression.codec", "snappy")
+
+# COMMAND ----------
+
+# raw_dataテーブルの結果確認
+
+display(
+  spark.read.format('delta').load('/tmp/masahiko.kitamura@databricks.com/raw_data.delta')
+)
+
+# COMMAND ----------
+
+# DBTITLE 1,Raw_data table -> image_processed_result table
+# [再掲]
+# UDFを定義(PILを使用して画像ファイルを開き、属性情報を抽出し、返す)
+@udf("width long, height long, mode string")
+def proc_image(image_binary):
+  import io
+  from PIL import Image
+  f=io.BytesIO(image_binary)
+  im = Image.open(f)
+  return (im.width, im.height, im.mode)
+
+
+
+# delta lakeからstreamingで読み込む (新規ファイルのみ読み込まれる)
+df_raw = (
+  spark.readStream.format('delta')
+  .load('/tmp/masahiko.kitamura@databricks.com/raw_data.delta')
+)
+
+
+# 画像処理(上記で定義したUDFを適用する)
+df_processed = (
+  df_raw
+  .withColumn('added_cols', proc_image('content')) # UDFの適用
+  .select(['path','added_cols.*']) # カラムのフラット化(`path`を外部キーにする想定)
+)
+
+
+# DeltaLakeに書き出す
+(
+  df_processed.writeStream.format('delta')
+  .option('checkpointLocation', '/tmp/masahiko.kitamura@databricks.com/image_processed_result.checkpoint')
+  .outputMode('append')
+  .trigger(once=True) # 一度だけ処理
+  .start('/tmp/masahiko.kitamura@databricks.com/image_processed_result.delta')
+  .awaitTermination() # async => sync
+)
+
+# COMMAND ----------
+
+#結果の確認
+
+display(
+  spark.read.format('delta').load('/tmp/masahiko.kitamura@databricks.com/image_processed_result.delta')
+)
+
+# COMMAND ----------
+
+# rawテーブルとのjoinで、パターンAと同じ結果を得る(python版)
+
+df_raw                    = spark.read.format('delta').load('/tmp/masahiko.kitamura@databricks.com/raw_data.delta')
+df_image_processed_result = spark.read.format('delta').load('/tmp/masahiko.kitamura@databricks.com/image_processed_result.delta')
+
+df_joined = (
+  df_raw.join( df_image_processed_result, df_raw.path == df_image_processed_result.path)
+)
+
+display(df_joined)
+
+
+# COMMAND ----------
+
+# rawテーブルとのjoinで、パターンAと同じ結果を得る(SQL版)
+
+df_raw                    = spark.read.format('delta').load('/tmp/masahiko.kitamura@databricks.com/raw_data.delta')
+df_image_processed_result = spark.read.format('delta').load('/tmp/masahiko.kitamura@databricks.com/image_processed_result.delta')
+
+df_raw.createOrReplaceTempView('table_raw')
+df_image_processed_result.createOrReplaceTempView('table_image_processed_result')
+
+df_joined = spark.sql('''
+  SELECT * FROM table_raw r
+  JOIN table_image_processed_result i
+  WHERE r.path = i.path
+''')
+
+display( df_joined )
+
+# COMMAND ----------
+
+
