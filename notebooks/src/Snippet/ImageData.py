04a4d9c25d3207465828d7bb76fc4e478769fafc
ktmrmshk <ktmrmshk>
2022-01-21 09:06:37 +0000

    updated

--- notebooks/src/Snippet/ImageData.py
+++ notebooks/src/Snippet/ImageData.py
@@ -4,7 +4,7 @@
 
 # COMMAND ----------
 
-# MAGIC %md ###Imageのプレビュー(方法1) - DataFrame + `display()`
+# MAGIC %md ###Imageのプレビュー(方法1) - DataFrame(`format="image"`) + `display()`
 # MAGIC 
 # MAGIC ----
 # MAGIC 
@@ -32,7 +32,33 @@ display(
 
 # COMMAND ----------
 
-# MAGIC %md ###Imageのプレビュー(方法2) - PIL + numpy + matplotlib
+# MAGIC %md ###Imageのプレビュー(方法2) - DataFrame(`format="binary"`) + `display()`
+# MAGIC 
+# MAGIC ---
+# MAGIC 
+# MAGIC * SparkのDataframe(`format="binaryFile"`)で読み込む
+# MAGIC * `.option("mimeType", "image/*")`でコンテンツ種別を指定
+# MAGIC * `display()`で表示する
+# MAGIC 
+# MAGIC ---
+# MAGIC 
+# MAGIC ドキュメント: [Binary File](https://docs.databricks.com/data/data-sources/binary-file.html)
+
+# COMMAND ----------
+
+image_df = (
+  spark
+  .read
+  .format("binaryFile")
+  .option("mimeType", "image/*")
+  .load('/databricks-datasets/cctvVideos/train_images/label=0/Browse2frame000*.jpg')
+)
+
+display( image_df ) 
+
+# COMMAND ----------
+
+# MAGIC %md ###Imageのプレビュー(方法3) - PIL + numpy + matplotlib
 # MAGIC ----
 # MAGIC 
 # MAGIC * 通常のJupyter Notebook上で実施するのと同じ方法
